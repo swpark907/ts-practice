386fdd0a70845e576a0fdabb83846987dcd48328
swpark907 <76856321+swpark907@users.noreply.github.com>
2021-10-16 23:21:04 +0900

    stack.ts study2

--- oop-project/stack.ts
+++ oop-project/stack.ts
@@ -9,29 +9,33 @@ type StackNode = {
     next?: StackNode | undefined;
 }
 
-class Stackimpl implements Stack{
-    private _size: number = 0;
-    private head? : StackNode;
-    get size(){
-        return this._size;
-    }
-    push<T>(value: T extends Stack): void{
-        this._size++;
-        const node: StackNode = {value, next: this.head}
-    };
-    pop(): string{
-        this._size--;
+class StackImpl implements Stack{
+    
+}
+
+// class Stackimpl implements Stack{
+//     private _size: number = 0;
+//     private head? : StackNode;
+//     get size(){
+//         return this._size;
+//     }
+//     push<T>(value: T extends Stack): void{
+//         this._size++;
+//         const node: StackNode = {value, next: this.head}
+//     };
+//     pop(): string{
+//         this._size--;
         
-    }
-}
+//     }
+// }
 
 
 
-const stack = new Stackimpl();
-stack.push('P1');
-stack.push('P2');
-stack.push('P3');
-while(stack.size !== 0){
-    console.log(stack.pop()); // --> P3 P2 P1 
-}
+// const stack = new Stackimpl();
+// stack.push('P1');
+// stack.push('P2');
+// stack.push('P3');
+// while(stack.size !== 0){
+//     console.log(stack.pop()); // --> P3 P2 P1 
+// }
 
