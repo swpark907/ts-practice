60839535d2322f03e1fc5735d1dab8ba7c4b6c68
swpark907 <76856321+swpark907@users.noreply.github.com>
2021-10-19 16:47:10 +0900

    stack.ts study finish

--- oop-project/stack.ts
+++ oop-project/stack.ts
@@ -1,58 +1,57 @@
 import { timeStamp } from "console";
 
 interface Stack{
-    _size: number;
+    readonly size: number;
     push(value: string): void;
     pop(): string;
 }
 
 type StackNode = {
-    value: string;
-    next?: StackNode | undefined;
+    readonly value: string;
+    readonly next?: StackNode;
 }
 
 class StackImpl implements Stack{
     
-    _size: number = 0;
+    private _size: number = 0; // 동일한 public변수가 있을 때 언더바_ 사용
     private head?: StackNode;
 
-    push(value: string): void{
+    // constructor(private capacity: number) { // 보통은 얼마만큼의 스택 사이즈를 가질 지 인자를 받음
+    //     if(this.size === this.capacity){
+    //         throw new Error('Stack is full!')
+    //     }
+    // }
+    get size(){
+        return this._size;
+    }
 
+    push(value: string): void{
+        const node: StackNode = {value, next: this.head};
+        this.head = node;    
         this._size++;
-        const node: StackNode = {value, next: this.head}
-        this.head = node;
     }
 
     pop(): string{
-        this._size--;
-        delete this.head?.next;
-        return 'string';
+        if(this.head == null){ // null == undefined, null !== undefined;
+            throw new Error('Stack is empty')
+        }
+        const node = this.head; // 이부분 처리 못했었음, node로 원래 값을 저장한 후 head를 직접 바꿔주면 됨
+        this.head = node.next;
+        this._size--;            
+        return node.value;
+        
     }
 }
 
-// class Stackimpl implements Stack{
-//     private _size: number = 0;
-//     private head? : StackNode;
-//     get size(){
-//         return this._size;
-//     }
-//     push<T>(value: T extends Stack): void{
-//         this._size++;
-//         const node: StackNode = {value, next: this.head}
-//     };
-//     pop(): string{
-//         this._size--;
-        
-//     }
-// }
 
 
+const stack = new StackImpl();
+stack.push('P1');
+stack.push('P2');
+stack.push('P3');
+while(stack.size !== 0){
+    console.log(stack.pop()); // --> P3 P2 P1 
+}
 
-// const stack = new Stackimpl();
-// stack.push('P1');
-// stack.push('P2');
-// stack.push('P3');
-// while(stack.size !== 0){
-//     console.log(stack.pop()); // --> P3 P2 P1 
-// }
+// stack.pop(); 한번더 할 경우 에러
 
