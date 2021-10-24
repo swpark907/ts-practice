f0581bf2342202b9616470daaba706b73f2bfd9f
swpark907 <76856321+swpark907@users.noreply.github.com>
2021-10-24 22:12:17 +0900

    8-2-index.ts study

---
+++ 8-advanced/8-2-index.ts
@@ -0,0 +1,31 @@
+{
+  const obj = {
+    name: 'ellie'
+  }
+
+  obj.name;
+  obj['name']
+
+  type Animal = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female'
+  }
+
+  type Name = Animal['name']; // string
+  const text: Name = 'hello';
+  
+  type Gender = Animal['gender'] // 'male' | 'female'
+  
+  type Keys = keyof Animal // 'name' | 'age' | 'gender';
+  const key: Keys = 'age';
+
+  type Person = {
+    name: string;
+    gender: Animal['gender']
+  }
+  const person: Person = {
+    name: 'ellie',
+    gender: 'male'
+  }
+}
